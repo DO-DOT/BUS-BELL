607bd66cfbbe26a612eada35e89b645c969fe08e
min <min0115b@gmail.com>
2022-11-14 07:24:51 +0900

    feat: Home Screen

--- App.js
+++ App.js
@@ -1,117 +1,186 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React from 'react'
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  	SafeAreaView,
+	ScrollView,
+	View,
+	TextInput,
+	Text,
+	Image,
+	TouchableOpacity,
+	FlatList,
+	StyleSheet,
+} from 'react-native'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+	return (
+		<SafeAreaView style={styles.container}>
+			{/* 검색바 */}
+			<View style={styles.searchContainer}>
+				<Image
+					style={styles.searchIcon}
+					source={require('./src/assets/img/search_icon.png')}
+				/>
+				<TextInput 
+					placeholder='버스 검색'
+				/>
+			</View>
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+			{/* 주변 정류장 */}
+			<TouchableOpacity
+				style={styles.nearbyStopContainer}
+				onPress={() => null}
+			>
+				<View>
+					<Text style={styles.nearbyStopText}>주변 정류장</Text>
+					<Text style={styles.nearbyStopNameText}>산학협력관</Text>
+				</View>
+				<Image
+					style={styles.pinIcon}
+					source={require('./src/assets/img/pin.png')}
+				/>
+			</TouchableOpacity>
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+			{/* 즐겨찾기 */}
+			<View style={styles.favoritesContainer}>
+				<Image
+					style={styles.starIcon}
+					source={require('./src/assets/img/star.png')}
+				/>
+				<Text style={styles.favoritesText}>즐겨찾기</Text>
+			</View>
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+			<ScrollView>
+				{/* 즐겨찾는 버스 */}
+				<View>
+					<TouchableOpacity
+						style={styles.favoritesBusContainer}
+						onPress={() => null}
+					>
+						<Text style={styles.favoritesBusNumText}>338</Text>
+						<Image
+							style={styles.busIcon}
+							source={require('./src/assets/img/bus.png')}
+						/>
+					</TouchableOpacity>
+					<TouchableOpacity
+						style={styles.favoritesBusContainer}
+						onPress={() => null}
+					>
+						<Text style={styles.favoritesBusNumText}>123</Text>
+						<Image
+							style={styles.busIcon}
+							source={require('./src/assets/img/bus.png')}
+						/>
+					</TouchableOpacity>
+					<TouchableOpacity
+						style={styles.favoritesBusContainer}
+						onPress={() => null}
+					>
+						<Text style={styles.favoritesBusNumText}>사하10</Text>
+						<Image
+							style={styles.busIcon}
+							source={require('./src/assets/img/bus.png')}
+						/>
+					</TouchableOpacity>
+				</View>
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+				{/* 즐겨찾기 추가 */}
+				<TouchableOpacity
+					style={styles.addFavoritesContainer}
+					onPress={() => null}
+				>
+					<Image
+						style={styles.plusIcon}
+						source={require('./src/assets/img/plus.png')}
+					/>
+					<Text>즐겨찾는 버스 추가하기</Text>
+				</TouchableOpacity>
+			</ScrollView>
+
+		</SafeAreaView>
+	)
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+	container: {
+		height: '100%',
+		backgroundColor: '#FFF',
+		padding: 20,
+	},
+	searchContainer: {
+		flexDirection: 'row',
+		backgroundColor: '#EEE',
+		borderRadius: 10,
+		alignItems: 'center',
+	},
+	searchIcon: {
+		height: 20,
+		width: 20,
+		marginHorizontal: 10,
+	},
+	nearbyStopContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		borderWidth: 1.5,
+		borderColor: '#FFD400',
+		borderRadius: 10,
+		padding: 15,
+		marginVertical: 20,
+	},
+	nearbyStopText: {
+		fontSize: 14,
+	},
+	nearbyStopNameText: {
+		fontSize: 17,
+		marginTop: 5,
+	},
+	pinIcon: {
+		height: 40,
+		width: 30,
+	},
+	favoritesContainer: {
+		flexDirection: 'row',
+		marginTop: 25,
+	},
+	starIcon: {
+		height: 20,
+		width: 20,
+		marginRight: 5,
+	},
+	favoritesText: {
+		fontSize: 15,
+	},
+	favoritesBusContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		borderWidth: 1.5,
+		borderColor: '#FFD400',
+		borderRadius: 10,
+		padding: 15,
+		marginTop: 15,
+	},
+	favoritesBusNumText: {
+		fontSize: 25,
+	},
+	busIcon: {
+		height: 35,
+		width: 35,
+	},
+	addFavoritesContainer: {
+		flexDirection: 'row',
+		borderWidth: 1.5,
+		borderColor: '#FFD400',
+		borderRadius: 10,
+		padding: 10,
+		marginVertical: 15,
+	},
+	plusIcon: {
+		height: 20,
+		width: 20,
+		marginRight: 5,
+	},
+})
 
-export default App;
+export default App
